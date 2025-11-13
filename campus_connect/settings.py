eeb76dbe71ce9235a20441b5e6d9d9ae92d74243
sarvjeet24kumar <sarvjeet@gkmit.co>
2025-11-13 18:52:46 +0530

    fix: removed SECRET_KEY and DEBUG_TRUE

--- campus_connect/settings.py
+++ campus_connect/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*bj__x@_y(%#gn2l!p88yui%dj+lyc@eswx*wj8h31)0irbmh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 
 
 ALLOWED_HOSTS = []
 
