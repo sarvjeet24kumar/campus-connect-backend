59f884bc3ec93b36af4a66e295a582bed4e1c3d9
sarvjeet24kumar <sarvjeet@gkmit.co>
2025-11-14 11:23:44 +0530

    fix: updated allowed host

--- campus_connect/settings.py
+++ campus_connect/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG").lower() == "true"
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
